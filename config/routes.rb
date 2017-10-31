994c8422909c627bb3470c91e57941020ec3d71d
Elise Barnes <barnese3@go.stockton.edu>
2017-10-31 13:18:39 -0400

    basic login, signup, and logout on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  resources :users
 get '/' => 'home#index', as: :root
 
   resources :jobs
   resources :boats
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
